e74646916a4c495216b171f7d1cb9c47b161e160
DIR-N-113 <DIR-N-113@DESKTOP-V2U4J5Q.mshome.net>
2022-01-20 15:41:35 +0900

    FileInputStreamTest commit

--- src/kr/co/jsh/test/steam/file/input/FileInputStreamTest.java
+++ src/kr/co/jsh/test/steam/file/input/FileInputStreamTest.java
@@ -11,9 +11,43 @@ public class FileInputStreamTest {
 		
 		try {
 			fileInputStream = new FileInputStream("C:\\dev\\test.txt");
+			
+			while(true) {
+				int read = fileInputStream.read();
+				if(read == -1) break;
+				char readChar = (char) read;
+				System.out.print(readChar);
+			}
+			
+			fileReader = new FileReader("C:\\dev\\test.txt");
+			while(true) {
+				int read = fileReader.read();
+				if(read == -1) break;
+				char readChar = (char) read;
+				System.out.print(readChar);
+			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} finally {
+			if(fileInputStream != null) {
+				try {
+					fileInputStream.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} else if(fileReader != null) {
+				try {
+					fileReader.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 		}
 				
 	}
